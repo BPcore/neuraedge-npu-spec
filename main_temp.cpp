ab1e5c239270f2bf6d1d645840bab480f7bfd8e0
BPcore <bpcoresilicon@gmail.com>
2025-07-26 17:52:14 +0530

    sim: add main_temp.cpp as simulation entry (workaround for git tracking)

---
+++ main_temp.cpp
@@ -0,0 +1,11 @@
+// File: main.cpp
+#include "Vneuraedge_top.h"
+#include "verilated.h"
+
+int main(int argc, char **argv) {
+    Verilated::commandArgs(argc, argv);
+    Vneuraedge_top *dut = new Vneuraedge_top;
+    // Optional: apply clock/reset stimulus here
+    delete dut;
+    return 0;
+}
